23c3e19e1aabb2aebd8cfbb5dc28f109637d2258
jun1orcarvalh0 <antoniomarcosdecj18@gmail.com>
2024-05-01 22:42:39 -0300

    adding time needed to buy tickets leetcode solution difficulty easy

---
+++ python/time_needed_to_buy_tickets.py
@@ -0,0 +1,111 @@
+"""leetcode link: https://leetcode.com/problems/time-needed-to-buy-tickets/
+
+There are n people in a line queuing to buy tickets, where the 0th person is at the front of the line and the (n - 1)th person is at the back of the line.
+
+You are given a 0-indexed integer array tickets of length n where the number of tickets that the ith person would like to buy is tickets[i].
+
+Each person takes exactly 1 second to buy a ticket. A person can only buy 1 ticket at a time and has to go back to the end of the line (which happens instantaneously) in order to buy more tickets. If a person does not have any tickets left to buy, the person will leave the line.
+
+Return the time taken for the person at position k (0-indexed) to finish buying tickets.
+
+ 
+
+Example 1:
+
+Input: tickets = [2,3,2], k = 2
+Output: 6
+Explanation: 
+- In the first pass, everyone in the line buys a ticket and the line becomes [1, 2, 1].
+- In the second pass, everyone in the line buys a ticket and the line becomes [0, 1, 0].
+The person at position 2 has successfully bought 2 tickets and it took 3 + 3 = 6 seconds.
+Example 2:
+
+Input: tickets = [5,1,1,1], k = 0
+Output: 8
+Explanation:
+- In the first pass, everyone in the line buys a ticket and the line becomes [4, 0, 0, 0].
+- In the next 4 passes, only the person in position 0 is buying tickets.
+The person at position 0 has successfully bought 5 tickets and it took 4 + 1 + 1 + 1 + 1 = 8 seconds.
+ 
+
+Constraints:
+
+n == tickets.length
+1 <= n <= 100
+1 <= tickets[i] <= 100
+0 <= k < n
+"""
+from collections import deque
+
+#first solution
+class Solution:
+    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
+        time = 0
+        
+        for i in range(0, k):
+            time += min(tickets[i], tickets[k])
+        
+        time += tickets[k]
+        
+        for i in range(k+1, len(tickets)):
+            time += min(tickets[k] - 1, tickets[i])
+
+        return time
+    
+#other solution
+class Solution:
+    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
+        time = 0
+        
+        for i in range(len(tickets)):
+            if i <= k:
+                time += min(tickets[k], tickets[i])
+            
+            else:
+                time += min(tickets[k] - 1, tickets[i])
+                
+        return time
+
+#other solution
+class Solution:
+    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
+        queue = deque()
+        
+        for i in range(len(tickets)):
+            queue.append(i)
+            
+        time = 0
+        
+        while queue:
+            time += 1
+            
+            front = queue.popleft()
+            
+            tickets[front] -= 1
+            
+            if k == front and tickets[front] == 0:
+                return time
+            
+            if tickets[front] != 0:
+                queue.append(front)
+        
+        return time
+
+#other solution
+class Solution:
+    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
+        n = len(tickets)
+        time = 0
+
+        if tickets[k] == 1:
+            return k + 1
+        
+        while tickets[k] > 0:
+            for i in range(n):
+                if tickets[i] != 0:
+                    tickets[i] -= 1
+                    time += 1
+
+                if tickets[k] == 0:
+                    return time
+        return time
